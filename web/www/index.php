c5e464d511e9ea6b20c73a4777c0216e401ce88a
Shitney <joshwhitney789@gmail.com>
2019-09-03 19:58:51 +1200

    Added some fancy new SQL calculations to the webpage to generate rankings. Also a search bar

--- web/www/index.php
+++ web/www/index.php
@@ -25,7 +25,14 @@ th, td {
   <th>Wins</th>
   <th>Losses</th>
   <th>Draws</th>
-  <th>Winrate</tr></tr>
+  <th>Total</th>
+  <th>Winrate</th>
+  <th>Score</th></tr>
+
+<form action="index.php" method="POST">
+  Search by Player Name: <input type="text" name="search">
+  <input type="submit">
+</form>
 
 <?php
  
@@ -36,16 +43,33 @@ $db_passwd = 'web_pw';
 $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
+$search = "".$_POST['search'];
+
 $q = $pdo->query(
   "SELECT ". 
   "id, win, loss, draw, ". 
-  "((win*2)+draw)/((win+loss+draw)*2)*100 AS winrate".
-  " FROM players");
+  "(win+loss+draw) AS total, ".
+  "((win*2)+draw)/((win+loss+draw)*2)*100 AS winrate, ".
+
+  // This is the score/ranking generating method
+  // Found at http://www.evanmiller.org/how-not-to-sort-by-average-rating.html
+  "((((win*2)+draw) + 1.9208) / (((win*2)+draw) + loss) - ".
+  "1.96 * SQRT((((win*2)+draw) * loss) / (((win*2)+draw) + loss) + 0.9604) / ".
+  "(((win*2)+draw) + loss)) / (1 + 3.8416 / (((win*2)+draw) + loss)) ".
+  "AS score ".
+
+  "FROM players ".
+  "WHERE id LIKE '%".$search."%'".
+  "ORDER BY score DESC");
 
 while($row = $q->fetch()){
-  echo "<tr><td>".$row["id"]."</td><td>".$row["win"]."</td>".
-  "<td>".$row["loss"]."</td><td>".$row["draw"]."</td>".
-  "<td>".$row["winrate"]."%</td></tr>\n";
+  echo "<tr><td>".$row["id"].
+       "</td><td>".$row["win"]."</td>".
+       "<td>".$row["loss"]."</td>".
+       "<td>".$row["draw"]."</td>".
+       "<td>".$row["total"]."</td>".
+       "<td>".$row["winrate"]."%</td>".
+       "<td>".$row["score"]."</td></tr>\n";
 }
 
 ?>
