5706bd7aa6c2f4c71fcd869ef47012abc0565a64
sh-tney <joshwhitney789@gmail.com>
2019-10-04 02:38:33 +1300

    Setting up credential config

---
+++ aws-credentials.sh
@@ -0,0 +1,8 @@
+# This file has empty fields in it which need to be filled with your aws credentials
+# These will be exported as shell variables, so that vagrant & awscli can use them
+# This file has been added to .gitignore specifically, such that even after editing this
+# 	file, your personal credentials will not be passed in any commits you make
+
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_SESSION_TOKEN=
