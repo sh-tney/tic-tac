04ae715ae99c2c7b9f7db3204ded4b5c53d5602f
sh-tney <joshwhitney789@gmail.com>
2019-10-04 14:39:42 +1300

    Difficulties with automating, switching over to manual configs on website

--- deconstruct.sh
+++ deconstruct.sh
@@ -1,15 +1,1 @@
-: '
-# Delete the S3 Bucket for the Client 
-echo "Deleting S3 Bucket Files..."
-aws s3api delete-object --bucket tic-tac-client \
-    --key ticTacClient.jar 
-echo "Deleting S3 Bucket..."
-aws s3api delete-bucket --bucket tic-tac-client --region us-east-1
-' 
-
-# Delete the VPC corresponding to the VPC_ID shell variable
-echo "Deleting VPC..."
-export VPC_ID=`cat vpc`
-aws ec2 delete-vpc --region us-east-1 --vpc-id $VPC_ID
-
-echo "Done."
+tic-tac-db-1.cfcpvj1lhbvf.us-east-1.rds.amazonaws.com
